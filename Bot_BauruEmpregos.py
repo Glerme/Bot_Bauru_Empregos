b07cf4b2164b4c28e45aafce3fc1b7644b12b537
Guilherme Godoi <62507710+Glerme@users.noreply.github.com>
2021-02-19 15:15:41 -0300

    Update Bot_BauruEmpregos.py

--- Bot_BauruEmpregos.py
+++ Bot_BauruEmpregos.py
@@ -8,10 +8,15 @@ from selenium.webdriver.common.action_chains import ActionChains
 import PySimpleGUI as sg  
 
 # Layout
-sg.theme('Dark Blue 3')
-layout = [  [sg.Text('Digite a vaga procurada: '),sg.Input(key='digitar',size=(40,30))],
-            [sg.Radio('Chrome',"SELECTED", default=True,key='chrome'),sg.Radio('Firefox',"SELECTED", default=False,key='firefox'),sg.Radio('Edge',"SELECTED", default=False,key='edge')],
-            [sg.OK('Começar',size=(20,2)), sg.Exit('Cancelar',key='cancel',size=(20,2))]
+# cor #8F8FBD #778899 #4B0082
+sg.theme_border_width(border_width='1')
+sg.theme_background_color(color='#34495E')
+layout = [  [sg.Text('Digite a vaga procurada: ', font='Arial 16 bold', text_color='#ffffff', background_color='#34495E'),
+                sg.Input(key='digitar', size=(30,30), font='Arial 16 bold')],
+            [sg.Radio('Chrome',"SELECTED", default=True, key='chrome', font='Arial 16 bold', background_color='#34495E', text_color='#1BA160'),
+                sg.Radio('Firefox',"SELECTED", default=False, key='firefox', font='Arial 16 bold', background_color='#34495E', text_color='#9B59B6'),
+                sg.Radio('Edge',"SELECTED", default=False, key='edge', font='Arial 16 bold', background_color='#34495E', text_color='#52B3D9')],
+            [sg.OK('Começar', size=(20,2), font='Arial 10 bold'), sg.Exit('Cancelar', key='cancel', size=(20,2), font='Arial 10 bold')],
         ] 
 
 # criar janela
@@ -61,10 +66,7 @@ while True:
             elif valores['edge'] == True:
                 driver = webdriver.Edge(EdgeChromiumDriverManager().install())
                 iniciar(driver)
-            else:
-                sg.popup_auto_close("Você não selecionou nenhum navegador!",title='Erro')
-
-                
+                                
         valor(valores)
 
     janela.close()
